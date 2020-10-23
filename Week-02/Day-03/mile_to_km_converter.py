668a30fcbcf4ecbf7f299444ad11c62324ce19e0
Andy-Vz <72815120+Andy-Vz@users.noreply.github.com>
2020-10-23 15:40:24 +0200

    Update mile_to_km_converter.py

--- Week-02/Day-03/mile_to_km_converter.py
+++ Week-02/Day-03/mile_to_km_converter.py
@@ -1,4 +1,4 @@
 miles = float(input("Enter the value in miles: "))
 conversion_factor = float (1.60934)
 kilometers = miles * conversion_factor
-print(kilometers + str("km))
+print(kilometers + str("km"))
