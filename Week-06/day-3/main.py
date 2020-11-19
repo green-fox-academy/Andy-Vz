63c3d68edfa7d6534ec17b5363f10bff743501a9
Andy-Vz <carlosandresav@hotmail.com>
2020-11-19 19:02:22 +0100

    Added support for checking a task, and also added error checking for it

--- Week-06/day-3/main.py
+++ Week-06/day-3/main.py
@@ -3,6 +3,8 @@ import pickle
 
 try:
     TasksFile = open('tasks.txt', 'rb')
+    # LoadedTasks contains the tasks and their Done status as a boolean
+    # it's structured like : [['Task Description',True],['Task2 Description',False]]
     LoadedTasks = pickle.load(TasksFile)
 except EOFError:
     LoadedTasks = []
@@ -22,7 +24,7 @@ elif "-a" in sys.argv:
     # before adding it to the list and saving to file
     try:
         TaskDescription = sys.argv[2]
-        LoadedTasks.append(TaskDescription)
+        LoadedTasks.append([TaskDescription,False])
         pickle.dump(LoadedTasks, open('tasks.txt', 'wb'))
     except IndexError:
         print("Unable to add: no task provided")
@@ -34,7 +36,10 @@ elif "-l" in sys.argv:
     else :
         for task in LoadedTasks:
             LoadedTaskIndex = str(LoadedTasks.index(task)+ 1)
-            print(LoadedTaskIndex + " - " + task)
+            if task[1]: # If the task is done
+                print(LoadedTaskIndex + " - [X] " + task[0])
+            else: # the task has not been completed yet
+                print(LoadedTaskIndex + " - [ ] " + task[0])
 elif "-r" in sys.argv:
     try:
         TaskNumber = sys.argv[2] # Task number to be removed
@@ -44,6 +49,20 @@ elif "-r" in sys.argv:
     try:
         LoadedTasks.pop(int(TaskNumber) - 1)  # Remove the task from the LoadedTasks list
         pickle.dump(LoadedTasks, open('tasks.txt', 'wb'))  # Write again to file
+    except IndexError:
+        print("Unable to remove: index is out of bound")
+    except ValueError:
+        print("Unable to remove: index is not a number")
+elif "-c" in sys.argv:
+    try:
+        TaskNumber = sys.argv[2]  # Task number to be checked
+    except IndexError: # Add error catching
+        print("Unable to remove: no index provided")
+        exit()
+    try:
+        TaskNumber = int(TaskNumber) - 1
+        LoadedTasks[TaskNumber][1] = True
+        pickle.dump(LoadedTasks, open('tasks.txt', 'wb'))
     except IndexError:
         print("Unable to remove: index is out of bound")
     except ValueError:
